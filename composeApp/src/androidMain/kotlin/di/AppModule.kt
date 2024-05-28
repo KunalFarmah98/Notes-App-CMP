db131e9a7a5c1ebfb4f8a1ed38d809b7c2dd4424
Kunal Farmah <kunalfarmah98@gmail.com>
2024-05-29 01:20:05 +0530

    fixed impl of AppModule

---
+++ composeApp/src/androidMain/kotlin/di/AppModule.kt
@@ -0,0 +1,21 @@
+package di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import database.NotesDatabase
+import org.koin.dsl.module
+
+// all injected items need to be returned here
+actual val appModule = module {
+    single {
+        getNotesDatabase(context = get()).noteDao()
+    }
+}
+
+fun getNotesDatabase(context: Context): NotesDatabase {
+    val dbFile = context.getDatabasePath("notes.db")
+    return Room.databaseBuilder<NotesDatabase>(context, dbFile.absolutePath)
+        .setDriver(BundledSQLiteDriver())
+        .build()
+}
--- composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -1,22 +1,5 @@
 package di
 
-import android.content.Context
-import androidx.room.Room
-import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import database.NotesDatabase
-import org.koin.dsl.module
+import org.koin.core.module.Module
 
-// all injected items need to be returned here
-val appModule = module {
-    single {
-        getNotesDatabase(context = get()).noteDao()
-    }
-}
-
-
-fun getNotesDatabase(context: Context): NotesDatabase {
-    val dbFile = context.getDatabasePath("notes.db")
-    return Room.databaseBuilder<NotesDatabase>(context, dbFile.absolutePath)
-        .setDriver(BundledSQLiteDriver())
-        .build()
-}
+expect val appModule: Module
